56b1f7d0cb2e8ff5e170ee7e6183c1b6e3066114
Anamika Verma <146219832+017-anamika@users.noreply.github.com>
2025-08-08 21:34:36 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,10 +13,18 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors({
-	origin: "https://code-world-afe6.onrender.com",
-	 credentials: true,
-}))
+const allowedOrigins = [
+  "https://code-world-beta.vercel.app",
+];
+
+// Use cors middleware
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
 
 app.use(express.static(path.join(__dirname, "public"))) // Serve static files
 
